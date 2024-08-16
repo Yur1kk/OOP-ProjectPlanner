1b931ac9645876bf6c6530f37e4e41f7710ac939
Yurii Soroka <urasor04@gmail.com>
2024-08-16 13:59:18 +0300

    Fully provided switch logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,20 +1,17 @@
-class Tooltip {
-
-}
+class Tooltip {}
 
 class ProjectItem {
-  constructor(id) {
+  constructor(id, updateProjectListsFunction) {
     this.id = id;
+    this.updateProjectListsHandler = updateProjectListsFunction;
     this.connectMoreInfoButton();
     this.connectSwitchButton();
   }
-  connectMoreInfoButton() {
-
-  }
+  connectMoreInfoButton() {}
   connectSwitchButton() {
     const projectItemElement = document.getElementById(this.id);
-    const switchBtn = projectItemElement.querySelector('button:last-of-type');
-    switchBtn.addEventListener('click', )
+    const switchBtn = projectItemElement.querySelector("button:last-of-type");
+    switchBtn.addEventListener("click", this.updateProjectListsHandler);
   }
 }
 
@@ -22,26 +19,39 @@ class ProjectList {
   projects = [];
 
   constructor(type) {
+    this.type = type;
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
     for (const prjItem of prjItems) {
-       this.projects.push(new ProjectItem(prjItem.id));
+      this.projects.push(
+        new ProjectItem(prjItem.id, this.switchProject.bind(this))
+      );
     }
   }
-  addProject() {
-   
+
+  setSwitchHandlerFunction(switchHandlerFunction) {
+    this.switchHandler = switchHandlerFunction;
   }
 
+  addProject() {}
+
   switchProject(projectId) {
-    const projectIndex = this.projects.findIndex(p => p.id === projectId);
+    this.switchHandler(this.projects.find((p) => p.id === projectId));
+    const projectIndex = this.projects.findIndex((p) => p.id === projectId);
     this.projects.splice(projectIndex, 1);
   }
 }
 
 class App {
-    static init () {
-      const activeProjectList = new ProjectList('active');
-      const finishedProjectList = new ProjectList('finished');
-    }
+  static init() {
+    const activeProjectList = new ProjectList("active");
+    const finishedProjectList = new ProjectList("finished");
+    activeProjectList.setSwitchHandlerFunction(
+      finishedProjectList.addProject.bind(finishedProjectList)
+    );
+    finishedProjectList.setSwitchHandlerFunction(
+      activeProjectList.addProject.bind(activeProjectList)
+    );
+  }
 }
 
 App.init();
